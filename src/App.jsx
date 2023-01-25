ddc2355b4d4c76b368aa399ea0464fc3f075bf1d
Andres Felipe Villarraga Arango <avillarraga@opc.com.co>
2023-01-25 18:02:11 -0500

    Inicio del proyecto

---
+++ src/App.jsx
@@ -0,0 +1,42 @@
+import React from "react";
+// Componentes
+import { TodoCounter }        from "./TodoCounter";
+import { TodoSearch }         from "./TodoSearch";
+import { TodoList }           from "./TodoList"
+import { TodoItem }           from "./TodoItem"
+import { CreateTodoButton }   from "./CreateTodoButton"
+// import './App.css';
+
+const todos = [
+  {
+    text:'Cortar cebollo',
+    completed:false
+  },
+  {
+    text:'Tomar cursos de React',
+    completed:false
+  },
+  {
+    text:'Dar comida a mis mascotas',
+    completed:false
+  },
+]
+
+
+
+function App() {
+  return (
+    <>
+    <TodoCounter/>  
+    <TodoSearch/>
+    <TodoList>
+        {todos.map(t => (
+          <TodoItem key={t.text} text={t.text}/>
+        ))}
+    </TodoList>
+    <CreateTodoButton/>
+    </>
+  )
+}
+
+export default App;
