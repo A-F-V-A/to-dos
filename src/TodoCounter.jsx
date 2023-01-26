f4cd99c237defa33ea6192838871c23c5cf0a49b
A-F-V-A <andresf.villarragaa@gmail.com>
2023-01-25 20:10:32 -0500

    Se crea estilo del buscador de TODOs

--- src/TodoCounter.jsx
+++ src/TodoCounter.jsx
@@ -3,7 +3,7 @@ import "./TodoCounter.css"
 
 const TodoCounter = () =>{
     return(
-        <h2 className="TodoCounter">Has completado 2 de 3 TODOs</h2>
+        <h2 className="TodoCounter">Has completado <span> 2 </span> de 3 TODOs</h2>
     )
 }
 
--- src/TodoSearch.jsx
+++ src/TodoSearch.jsx
@@ -1,9 +1,15 @@
 import React from "react";
+import "./TodoSearch.css"
 
 const TodoSearch = () =>{
     return (
-        <input type="text" placeholder="Cebolla" />
+        <div className="TodoSearch">
+            <input type="text" placeholder="Buscar mis TODOs" />
+            <span className="material-symbols-outlined">
+                search
+            </span>
+        </div>
     )
 }
 
-export {  TodoSearch }
+export { TodoSearch }
